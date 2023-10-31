405003c937c36a4eff1b7d89a6152380c932ad81
Oskar <o.pulczynski@gmail.com>
2023-10-31 15:25:14 +0100

    Learning JavaScript
    Objects and Fuctions

--- script/main.js
+++ script/main.js
@@ -8,3 +8,37 @@ const header = document.querySelector(".page-header__heading--js");
 console.log(header.innerHTML);
 
 header.innerHTML = `Siema jestem ${firstName} i mam ${age} lat.`;
+
+const calculate = (myNumber) => {
+myNumber = myNumber +3;
+ console.log(myNumber);
+  return myNumber*7;
+} 
+calculate(2)
+calculate (3)
+calculate(5)
+const result = calculate(5)
+console.log(result)
+
+
+function hello() {
+    console.log('hello')
+}
+
+hello()
+
+const deathStar = {
+    diameter: 120000,
+    fire: (target) => {
+        console.log(`${target} destroyed 💥`)
+    },
+    isOperating: true,
+    levels: 357,
+    name: 'Death Star',
+    population: 10000,
+    commander: {
+        name: 'Darth Baldshroom'
+    }
+}
+deathStar.fire('sąsiad')
+console.log(deathStar.name)
