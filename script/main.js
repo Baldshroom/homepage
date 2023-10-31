65e64a22ca2164232f23c6204e80b581fb388ee8
Oskar <o.pulczynski@gmail.com>
2023-10-31 07:51:05 +0100

    Learning JavaScript. BEM first steps
    -const
    -let
    -name.innerHTML

--- script/main.js
+++ script/main.js
@@ -2,3 +2,9 @@ const firstName = "Oskar";
 const age = "25";
 
 console.log(`Cześć, nazywam się ${firstName} i mam ${age} lat`);
+
+const header = document.querySelector(".page-header__heading--js");
+
+console.log(header.innerHTML);
+
+header.innerHTML = `Siema jestem ${firstName} i mam ${age} lat.`;
